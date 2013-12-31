f4c7210cbfed162da8482d5f78509235b9eb4b87
David Ruyer <david.ruyer@gmail.com>
2013-12-31 03:04:37 +0100

    redistogo integration

--- api.rb
+++ api.rb
@@ -4,7 +4,9 @@ require 'redis'
 
 class Api < Sinatra::Base
 
-  REDIS = Redis.new
+  redis_uri = URI.parse(ENV["REDISTOGO_URL"])
+  REDIS = Redis.new(:host => redis_uri.host, :port => redis_uri.port, :password => redis_uri.password)
+
   TRACKS_KEY = 'tracks'
 
   get '/vote' do
